176a8b960aa6de552b8f0ce194f46be0916357e4
Debiddo618 <dzheng@terpmail.umd.edu>
2025-01-09 10:00:51 -0500

    Changed text color of the navbar based on route path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
-import { Link, useLocation} from '@tanstack/react-router'
-import { Heading1 } from 'lucide-react';
+import { Link, useLocation } from "@tanstack/react-router";
 
 export default function Navbar() {
   interface User {
@@ -7,7 +6,7 @@ export default function Navbar() {
     name: string;
     email: string;
   }
-  
+
   let user: User | any = {
     id: 1,
     name: "John Doe",
@@ -16,53 +15,68 @@ export default function Navbar() {
 
   user = null;
 
-  // this is used to check if user is either on the login or register page to conditionally apply styles to the navbar
+  // Check if the user is on the login or register page
   const location = useLocation();
-  const isLoginPage = location.pathname === '/login';
-  const isRegisterPage = location.pathname === '/register';
+  const isLoginPage = location.pathname === "/login";
+  const isRegisterPage = location.pathname === "/register";
   const isAuth = isLoginPage || isRegisterPage;
 
+  const linkTextColor = isAuth ? "text-white" : "text-[#022b60]";
+
   return (
-    <div className={`flex items-center justify-between h-24 py-3 px-36 ${user ? 'shadow-md' : ''} ${isAuth ? 'bg-transparent':'bg-[#022b60]'}`}>
+    <div
+      className={`flex items-center justify-between h-24 py-3 px-36 ${user ? "shadow-md" : ""} ${isAuth ? "bg-transparent" : ""}`}
+    >
       <div className="flex items-center justify-center">
         <Link to="/HomePage">
-          <img src="logo-white.png" alt="Logo" className='w-[75px] h-[75px]' />
+          <img
+            src={isAuth ? "logo-white.png" : "logo-blue.png"}
+            alt="Logo"
+            className="w-[75px] h-[75px]"
+          />
         </Link>
       </div>
-      {user &&
+      {user && (
         <div className="flex gap-3 ml-3 flex-1">
-          <Link to="/" className="[&.active]:font-bold">
+          <Link to="/" className={`${linkTextColor} [&.active]:font-bold`}>
             Hotels
           </Link>
-          <Link to="/" className="[&.active]:font-bold">
+          <Link to="/" className={`${linkTextColor} [&.active]:font-bold`}>
             Reservations
           </Link>
-          <Link to="/" className="[&.active]:font-bold">
+          <Link to="/" className={`${linkTextColor} [&.active]:font-bold`}>
             Reviews
           </Link>
         </div>
-      }
+      )}
 
-      {user && 
+      {user && (
         <>
           <div className="bg-orange-500 mx-3">
-            <input type="text" placeholder='Enter a location' />
+            <input type="text" placeholder="Enter a location" />
           </div>
           <div className="flex items-center gap-3">
             <div className="w-[50px] h-[50px] rounded-full bg-blue-300"></div>
             <span>{user?.name}</span>
           </div>
         </>
-      }
-      {!user && <div className='flex gap-3'>
-        <Link to="/login" className="[&.active]:font-bold text-white text-xl">
-          Login
-        </Link>
-        <Link to="/register" className="[&.active]:font-bold text-white text-xl">
-          Register
-        </Link>
-      </div>}
-        
+      )}
+      {!user && (
+        <div className="flex gap-3">
+          <Link
+            to="/login"
+            className={`${linkTextColor} [&.active]:font-bold text-xl`}
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className={`${linkTextColor} [&.active]:font-bold text-xl`}
+          >
+            Register
+          </Link>
+        </div>
+      )}
     </div>
-  )
+  );
 }
