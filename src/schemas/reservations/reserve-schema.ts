abddf416a2558dd0758f8e7787a5f546e5b99da2
Waleed Mustafa Qureshi <129791489+WaleedQureshi141@users.noreply.github.com>
2025-01-13 14:25:59 -0500

    new reservation working

--- src/schemas/reservations/reserve-schema.ts
+++ src/schemas/reservations/reserve-schema.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const reserveFormSchema = z.object(
     {
-        user_id: z.string(),
-        check_in_time: z.string(),
-        check_out_time: z.string(),
+        userId: z.number(),
+        checkInTime: z.string(),
+        checkOutTime: z.string(),
     }
 );
 
