16c2cfc101b9a2cc3a50221cccf3796e063e1510
Waleed Mustafa Qureshi <129791489+WaleedQureshi141@users.noreply.github.com>
2025-01-14 16:37:01 -0500

    landing page shows correct hotels

--- src/hooks/users/auth/use-logout.ts
+++ src/hooks/users/auth/use-logout.ts
@@ -1,6 +1,6 @@
 import { useRouter } from "@tanstack/react-router";
 import { toast } from "../../use-toast";
-import { allHotelReviewsAtom, allHotelsAtom, allReserveAtom, hotelAtom, hotelIdAtom, reserveAtom, reviewAtom, userAtom } from "@/store/atoms";
+import { allHotelReviewsAtom, allReserveAtom, reserveAtom, reviewAtom, userAtom } from "@/store/atoms";
 import { useSetAtom } from "jotai";
 
 export function useLogout() {
@@ -9,24 +9,18 @@ export function useLogout() {
   const setUser = useSetAtom(userAtom);
   const setReserve = useSetAtom(reserveAtom);
   const setReview = useSetAtom(reviewAtom);
-  const setHotel = useSetAtom(hotelAtom);
-  const setHotelId = useSetAtom(hotelIdAtom);
 
   const setAllReview = useSetAtom(allHotelReviewsAtom);
   const setAllReserve = useSetAtom(allReserveAtom);
-  const setAllHotel = useSetAtom(allHotelsAtom);
 
   const logout = () => {
     localStorage.removeItem("token");
     setUser(null);
     setReserve(null);
     setReview(null);
-    setHotel(null);
-    setHotelId(null);
     
     setAllReview([]);
     setAllReserve([]);
-    setAllHotel([]);
 
     toast({
       title: "Logged out successfully",
