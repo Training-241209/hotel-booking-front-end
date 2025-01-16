9b7546a857c4715117e643d6118e65a88a1e40ef
Waleed Mustafa Qureshi <129791489+WaleedQureshi141@users.noreply.github.com>
2025-01-16 11:46:59 -0500

    landing bar working but homepage bar error

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,8 @@
 import HotelCarousel from "@/components/Hotel/HotelCarousel";
 import { Input } from "@/components/ui/input";
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { filterWordAtom } from "@/store/atoms";
+import { createLazyFileRoute, useRouter } from "@tanstack/react-router";
+import { useAtom } from "jotai";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
@@ -8,6 +10,23 @@ export const Route = createLazyFileRoute("/")({
 
 function Index() 
 {
+  const [filterHotel, setFilterHotel] = useAtom(filterWordAtom);
+  const router = useRouter();
+  
+  const handleChange = (e: any) => {
+    setFilterHotel(e.target.value);
+  };
+
+  // checks if "Enter" key has been pressed
+  const onEnterPress = (e: React.KeyboardEvent<HTMLInputElement>) =>
+  {
+    if (e.key === "Enter")
+    {
+      console.log(`filtered hotel: ${filterHotel}`);
+      router.navigate({to: "/HomePage"});
+    }
+  }
+
   return (
     <div className="col-span-full row-start-2 row-end-13 grid grid-cols-10 grid-rows-10">
       <div className="logo col-start-5 col-end-7 mx-auto row-span-2 xs:col-start-3 xs:col-end-9 xs:row-start-2 
@@ -21,6 +40,9 @@ function Index()
           type="text"
           placeholder="Location Search"
           className="rounded-lg shadow-sm"
+          value={filterHotel ? filterHotel : ""}
+          onChange={handleChange}
+          onKeyDown={onEnterPress}
         />
       </div>
       <div className="hotel_carousel row-start-7 col-start-0 col-span-full row-end-11">
