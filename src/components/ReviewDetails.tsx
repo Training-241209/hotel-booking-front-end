f2ae5ad4359ae611b326fb52998c25a7a3623439
Debiddo618 <dzheng@terpmail.umd.edu>
2025-01-15 22:51:50 -0500

    only allow owner to delete reviews

--- src/components/ReviewDetails.tsx
+++ src/components/ReviewDetails.tsx
@@ -7,16 +7,17 @@ interface ReviewDetailsProps{
   rating: number;
   userFN: string;
   userLN: string;
+  userId: any;
 }
 
-export default function ReviewDetails({reviewId,title,description,rating,userFN, userLN}:ReviewDetailsProps) {
+export default function ReviewDetails({reviewId,title,description,rating,userFN, userLN, userId}:ReviewDetailsProps) {
   
   // console.log(`firstname ${userFN}`);
 
   return (
     <div className="flex h-full flex-col">
       <h1 className="font-bold">Most Recent Review</h1>
-      <ReviewItem reviewId={reviewId} title={title} description={description} rating={rating} userFN={userFN} userLN={userLN} show={true} />
+      <ReviewItem reviewId={reviewId} title={title} description={description} rating={rating} userFN={userFN} userLN={userLN} show={true} userId={userId} />
     </div>
   );
 }
